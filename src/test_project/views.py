a5188d80ddc2169a080c7b36e1e85e6a00a72bcd
exy_p <exy_pret@gmail.com>
2021-02-12 19:27:47 +0200

    first commit

---
+++ src/test_project/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+import datetime
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'Misha'
+    received_info = {'date': date, 'name': name}
+    return render(request, 'home.html', received_info)
